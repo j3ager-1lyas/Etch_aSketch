ae321cb62404f832662eb589c5c1abe901aaa391
Ilyas Elasri <ilyas.elasri.unreal@gmail.com>
2022-05-01 19:04:05 +0000

    added initialisation and resolution

--- javascript.js
+++ javascript.js
@@ -1,33 +1,84 @@
-const sketch=document.querySelector('#sketchboard');
+/*getting selected Resolution Activly*/
+const resoSlider=document.querySelector('#resoSlider');
 const board=document.querySelector('#board');
+let resolution=resoSlider.value;
+resoSlider.addEventListener('change',()=>{getResoValue();createBoard()})
+
+function getResoValue(){
+        resolution=resoSlider.value;
+}
+/*setting board Resolution */
+const sketchboard= document.querySelector('#sketchboard');
 let divvert=[];
-let divhorz=[];
-sketch.style.height=board.clientHeight;
-sketch.style.width=board.clientWidth;
-for(i=0;i<16;i++){
-    divvert[i]=document.createElement('div');
-    divvert[i].id="verticalpixel";
-    divvert[i].style.height=(board.clientHeight)/16+'px';
-    sketch.appendChild(divvert[i])
-    for(j=0;j<16;j++){
-        divhorz[j]=document.createElement('div');
-        divhorz[j].id="horizentalpixel";
-        divhorz[j].style.width=(board.clientWidth)/16+'px';
-        divvert[i].appendChild(divhorz[j]);
+let divvhorz=[];
+let swidth=sketchboard.clientWidth;
+let sheight=sketchboard.clientHeight;
+let childs=sketchboard.children;
+let newresolution=resolution-(childs.length);
+let lines=0;
+let pixels=0;
+createBoard();
+function pixelColoring(){
+    pixels.forEach((pixel)=>{
+        pixel.addEventListener('mouseover',(e)=>{coloring(e);})
+    })
+    function coloring(e){
+        e.target.style.backgroundColor='black';
     }
 }
-const pixels=document.querySelectorAll('#horizentalpixel');
-pixels.forEach((pixel)=>{
-    pixel.addEventListener('mouseover', (e)=>{
-        
-            let col=e.target;
-            col.style.backgroundColor='black'
-            console.log(col);
-       
-    });
+function initializeColoring(){
+    pixels=document.querySelectorAll('#pixel');
+    pixels.forEach(function (pixel){
+        pixel.style.backgroundColor='white';
+    })
     
-})
+}
+function createBoard(){
+    initializeColoring();
+        swidth=sketchboard.clientWidth;
+        sheight=sketchboard.clientHeight;
+        childs=sketchboard.children;
+        newresolution=resolution-(childs.length)
+        /**addingpixels */
+        if (childs.length<=resolution){addPixels();}
+        /**removingpixels */
+        else {removePixels();}
+        function addPixels()
+        {
+            for(i=0;i<newresolution;i++){
+                divvert[i]=document.createElement('div');
+                divvert[i].id='lines';
+                sketchboard.appendChild(divvert[i]);
+                    for(j=0;j<newresolution;j++){
+                        divvhorz[j]=document.createElement('div');
+                        divvhorz[j].id="pixel";
+                        divvert[i].appendChild(divvhorz[j]);
+                    }
 
+            }
+            addaptPixelSize();
+        }
+        function removePixels(){
+            for(i=0;i<-newresolution;i++)
+            {
+                sketchboard.removeChild(sketchboard.firstChild)
+            }
+            addaptPixelSize();
+        }
+        function addaptPixelSize(){
+            lines=document.querySelectorAll('#lines');
+            lines.forEach((line)=>{
+                line.style.display="flex";
+                line.style.height= sheight/resolution+'px'
+            })
+            pixels=document.querySelectorAll('#pixel');
+            pixels.forEach((pixel)=>{
+                pixel.style.width=swidth/resolution +'px';
+                pixel.style.height=sheight/resolution+'px';
+            })
+        }
+pixelColoring();
+}
 
 
 
