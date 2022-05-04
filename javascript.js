6b0dc6432cc7365d5334b1fa1745e917e302fba6
Ilyas Elasri <ilyas.elasri.unreal@gmail.com>
2022-05-04 21:36:29 +0000

    update the code

--- javascript.js
+++ javascript.js
@@ -1,4 +1,66 @@
+/*creating a Board*/
+window.addEventListener('resize',setPSize)
+let currentResolution=0;
+let selectedResolution=0;
+let pixels=[];
+let difference=0;
+const sketchboard=document.querySelector('#sketchboard');
+const resoSlider=document.querySelector('#resoSlider');
+resoSlider.addEventListener('change',function (e){
+    selectedResolution=e.target.value;
+    createBoard();
+    const resoText=document.querySelector('#resoLabel');
+    resoText.textContent=selectedResolution+'x'+selectedResolution;
+    console.log(sketchboard.childElementCount);
+})
+function removePixels(difference){
+    let nClear=sketchboard.childElementCount;
+    for(i=0;i<nClear;i++){
+        sketchboard.removeChild(sketchboard.lastElementChild);
+        pixels.pop();
+    }
+}
+function changeColor(e){
+    if (e.type=='mouseover'){
+        e.target.style.backgroundColor='black';
+    }
+}
+function addPixels()
+        {   
+            let currentIndex=(pixels.length);
+            for(i=0;i<selectedResolution**2;i++,currentIndex++)
+            {   
+                pixels[currentIndex]=document.createElement('div');
+                pixels[currentIndex].id='pixel';
+                pixels[currentIndex].addEventListener('mouseover',changeColor);
+                pixels[currentIndex].addEventListener('mousedown',changeColor);
+                sketchboard.appendChild(pixels[currentIndex]);
+            }
+            setPSize();
+            console.log("added"+difference)
+        }
+function setPSize(){
+     sketchboard.style.height=sketchboard.clientWidth+'px';
+     let pixel=document.querySelectorAll('#pixel');
+     pixel.forEach((square)=>{
+         square.style.width= sketchboard.clientWidth/selectedResolution +'px';
+         square.style.height= sketchboard.clientHeight/selectedResolution+'px';
+     })
+    
+ }
+
+
+function createBoard(){
+    
+        removePixels();
+   
+        addPixels();
+     
+        currentResolution=selectedResolution;
+        
+}
 /*getting selected Resolution Activly*/
+/*-----------------------
 const resoSlider=document.querySelector('#resoSlider');
 const board=document.querySelector('#board');
 let resolution=resoSlider.value;
@@ -7,7 +69,7 @@ resoSlider.addEventListener('change',()=>{getResoValue();createBoard()})
 function getResoValue(){
         resolution=resoSlider.value;
 }
-/*setting board Resolution */
+/*setting board Resolution 
 const sketchboard= document.querySelector('#sketchboard');
 let divvert=[];
 let divvhorz=[];
@@ -17,6 +79,7 @@ let childs=sketchboard.children;
 let newresolution=resolution-(childs.length);
 let lines=0;
 let pixels=0;
+createPixels();
 createBoard();
 function pixelColoring(){
     pixels.forEach((pixel)=>{
@@ -33,34 +96,44 @@ function initializeColoring(){
     })
     
 }
+function createPixels(){
+            for(i=0;i<resoSlider.max;i++){
+                divvert[i]=document.createElement('div');
+                divvert[i].id='lines';
+                    for(j=0;j<resoSlider.max;j++){
+                        divvhorz[j]=document.createElement('div');
+                        divvhorz[j].id="pixel";        
+                    }
+            }
+}
 function createBoard(){
     initializeColoring();
         swidth=sketchboard.clientWidth;
         sheight=sketchboard.clientHeight;
         childs=sketchboard.children;
         newresolution=resolution-(childs.length)
-        /**addingpixels */
+        /**addingpixels 
+        
         if (childs.length<=resolution){addPixels();}
-        /**removingpixels */
+        /**removingpixels 
         else {removePixels();}
-        function addPixels()
-        {
+        function addPixels(){
             for(i=0;i<newresolution;i++){
-                divvert[i]=document.createElement('div');
-                divvert[i].id='lines';
                 sketchboard.appendChild(divvert[i]);
                     for(j=0;j<newresolution;j++){
-                        divvhorz[j]=document.createElement('div');
-                        divvhorz[j].id="pixel";
-                        divvert[i].appendChild(divvhorz[j]);
+                    divvert[i].appendChild(divvhorz[j]);     
                     }
-
             }
+            
+            
             addaptPixelSize();
         }
         function removePixels(){
             for(i=0;i<-newresolution;i++)
             {
+                for(j=0;j<-newresolution;j++){
+                    divvert.removeChild(divvert.firstChild);     
+                    }
                 sketchboard.removeChild(sketchboard.firstChild)
             }
             addaptPixelSize();
@@ -70,6 +143,7 @@ function createBoard(){
             lines.forEach((line)=>{
                 line.style.display="flex";
                 line.style.height= sheight/resolution+'px'
+                line.style.width=swidth;
             })
             pixels=document.querySelectorAll('#pixel');
             pixels.forEach((pixel)=>{
@@ -79,6 +153,6 @@ function createBoard(){
         }
 pixelColoring();
 }
-
-
+/**make sure to check the bugs of resolution, unused space(probably bcz of the pixel element call) 
+------*/
 
